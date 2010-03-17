bfbd50aff17540505fde46ba1e7dc56a37f0dab7
partow <partow@9d6d256b-b519-0410-bfcb-7781182a7f52>
2010-03-17 03:04:46 +0000

    C++ Open Bloom Filter Library  http://www.partow.net/programming/hashfunctions/index.html

--- bloom_filter.hpp
+++ bloom_filter.hpp
@@ -101,7 +101,7 @@ public:
       inserted_element_count_ = 0;
    }
 
-   inline void insert(const unsigned char* key_begin, const std::size_t length)
+   inline void insert(const unsigned char* key_begin, const std::size_t& length)
    {
       std::size_t bit_index = 0;
       std::size_t bit = 0;
